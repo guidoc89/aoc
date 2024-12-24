a391eb8cea27c37af4e62df2d68009a37190a6af
guido <guid.castr@gmail.com>
2024-12-24 18:22:07 -0300

    feat(rust): 2024 day3 sol

--- src/utils/year.rs
+++ src/utils/year.rs
@@ -45,7 +45,7 @@ fn match_day_in_year_2024(day: &str, input_path: String) -> SolutionPair {
     match day {
         "day1" => year2024::day1::day1::solve(&input_path),
         "day2" => year2024::day2::day2::solve(&input_path),
-        // "day3" => year2024::day3::day3::solve(&input_path),
+        "day3" => year2024::day3::day3::solve(&input_path),
         // "day4" => year2024::day4::day4::solve(&input_path),
         _ => panic!("Day not defined")
     }
--- src/year2024/day3/day3.rs
+++ src/year2024/day3/day3.rs
@@ -1,3 +1,110 @@
-pub fn solve (file_path: &str) {
-    println!("Solution from 2024 day3 - Mode: {:?}", file_path);
+use std::cell::RefCell;
+
+use crate::utils::file;
+use crate::utils::solution::{Solution, SolutionPair};
+
+const PAT: &str = "mul(";
+
+// NOTE: just another way
+// fn part1(file_string: String) -> i32 {
+//     let mut ans = 0;
+//     let file_string = file_string.replace("\n", "");
+//     for (idx,c) in file_string.chars().enumerate() {
+//         let mut j = idx;
+//         if file_string[idx..].starts_with(PAT) {
+//             while file_string.chars().nth(j).unwrap() != ')' {
+//                 j += 1
+//             }
+//
+//             let possible = &file_string[idx..=j].replace(PAT, "").replace(")", "");
+//             let splits: Vec<_> = possible.split(",").collect();
+//
+//                 if splits.len() == 2 {
+//                     if let (Ok(num1), Ok(num2)) = (splits[0].trim().parse::<i32>(), splits[1].trim().parse::<i32>()) {
+//                         ans += (num1 * num2);
+//                     }
+//                 }
+//         }
+//     }
+//     ans
+// }
+
+fn part1(file_string: String) -> i32 {
+    file_string
+        .replace("\n", "")
+        .chars()
+        .enumerate()
+        .filter_map(|(idx, _)| {
+            if file_string[idx..].starts_with(PAT) {
+                let j = file_string[idx..]
+                    .chars()
+                    .position(|c| c == ')')
+                    .map(|pos| pos + idx)?;
+
+                let possible = &file_string[idx..=j].replace(PAT, "").replace(")", "");
+                let splits: Vec<&str> = possible.split(",").collect();
+                if splits.len() == 2 {
+                    if let (Ok(n1), Ok(n2)) = (
+                        splits[0].trim().parse::<i32>(),
+                        splits[1].trim().parse::<i32>(),
+                    ) {
+                        return Some(n1 * n2);
+                    }
+                }
+            }
+            None
+        })
+        .sum()
+}
+
+// NOTE: this works, but find another way to solve it without the mutable RefCell inside the filter_map closure, drop the filter_map
+// and handle it differently
+fn part2(file_string: String) -> i32 {
+    let enabled = RefCell::new(true);
+
+    file_string
+        .replace("\n", "")
+        .chars()
+        .enumerate()
+        .filter_map(|(idx, _)| {
+            if file_string[idx..].starts_with("do()") {
+                *enabled.borrow_mut() = true;
+                return None;
+            }
+            if file_string[idx..].starts_with("don't()") {
+                *enabled.borrow_mut() = false;
+                return None;
+            }
+
+            if file_string[idx..].starts_with(PAT) {
+                let j = file_string[idx..]
+                    .chars()
+                    .position(|c| c == ')')
+                    .map(|pos| pos + idx)?;
+
+                let possible = &file_string[idx..=j].replace(PAT, "").replace(")", "");
+                let splits: Vec<&str> = possible.split(",").collect();
+                if *enabled.borrow() {
+                    if splits.len() == 2 {
+                        if let (Ok(n1), Ok(n2)) = (
+                            splits[0].trim().parse::<i32>(),
+                            splits[1].trim().parse::<i32>(),
+                        ) {
+                            return Some(n1 * n2);
+                        }
+                    }
+                }
+            }
+            None
+        })
+        .sum()
+}
+
+pub fn solve(file_path: &str) -> SolutionPair {
+    let file_string = file::read_file(file_path);
+
+    let sol1 = part1(file_string.clone());
+    let sol2 = part2(file_string.clone());
+
+    (Solution::from(sol1), Solution::from(sol2))
 }
