7be02e76105549d3af52e90c64596945e712fedf
guido <guid.castr@gmail.com>
2025-01-16 01:56:21 +0100

    feat(scala): 2024 day7 sol

---
+++ src/year2024/day7/scala/part1.scala
@@ -0,0 +1,38 @@
+
+
+@main def part1(): Long = {
+  var lines = scala.io.Source.fromFile("../prod1.txt").getLines().toList
+  var ans: Long = 0
+
+  for (line <- lines) {
+    val Array(left, right) = line.split(":").map(_.trim)
+    val target = left.toLong
+    //
+    // Splits by ANY amount of whitespace
+    val nums = right.split("\\s+").map(_.toInt).toList
+
+
+    if (Helpers.isLineCorrect(nums, 0, 0, target)) {
+      ans += target
+    }
+  }
+
+
+
+  ans
+}
+
+object Helpers {
+  def isLineCorrect(nums: List[Int], idx: Int, currVal: Long, target: Long): Boolean = {
+    if (idx == nums.length) {
+      return currVal == target
+
+    }
+
+
+    var addRes = isLineCorrect(nums, idx+1, currVal+nums(idx), target)
+    var mulRes = isLineCorrect(nums, idx+1, currVal*nums(idx), target)
+
+    return addRes || mulRes
+  }
+}
